150a8cb3c5a9234307567749841b154ec6959794
kthss01 <kthss01@naver.com>
2020-07-23 01:22:34 +0900

    템플릿 메소드 패턴 (Template Method Pattern) 알고리즘의 구조를 메소드에 정의하고 하위 클래스에서 알고리즘 구조의 변경없이 알고리즘을 재정의 하는 패턴
    사용하는 경우
    구현하려는 알고리즘이 일정한 프로세스가 있다.
    구현하려는 알고리즘이 변경 가능성이 있다.
    사용하는 방법
    알고리즘을 여러 단계로 나눈다.
    나눠진 알고리즘의 단계를 메소드로 선언한다.
    알고리즘을 수행할 템플릿 메소드를 만든다.
    하위 클래스에서 나눠진 메소드들을 구현한다

---
+++ TemplateMethod/src/Main.java
@@ -0,0 +1,12 @@
+import com.kay.dp.AbstGameConnectHelper;
+import com.kay.dp.DefaultGameConnectHelper;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		AbstGameConnectHelper helper = new DefaultGameConnectHelper();
+		helper.requestConnection("아이디 암호 등 접속 정보");
+	}
+
+}
